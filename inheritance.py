d3c1f3ac819f2fc9df250f4ef0ad355acdb5ff3c
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-07-28 11:41:27 -0400

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class Bird():
+	size = 0
+	wingspan = 0
+
+class Birb(Bird):
+	size = "small"
+	roundedness = 0
+	cuteness = 0
+	fluffiness = 0
+
+class Borb(Bird):
+	roundedness = "complete"
+	muppetness = 0
