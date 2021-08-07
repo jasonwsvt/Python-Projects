e79b0baa4b87d0a36681e837057ffb260cd69f6d
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-08-06 21:43:38 -0400

    Django_Checkbook project

---
+++ Django_Checkbook/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jason\Documents\GitHub\Python-Projects\Django_Checkbook\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Django_Checkbook/src/Django_Checkbook/Checkbook/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .forms import AccountForm, TransactionForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+def create_account(request):
+    form = AccountForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/CreateNewAccount.html')
+
+def balance(request):
+    return render(request, 'checkbook/BalanceSheet.html')
+
+def transaction(request):
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/AddTransaction.html')
